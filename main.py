7bec37bf3316fe7f6a8b9857ab730b6af6ad99a3
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-21 17:22:38 +0100

    PEMDAS

---
+++ main.py
@@ -0,0 +1,15 @@
+#1 part
+x = 2
+y = 10
+z = 7
+p = 3
+q = 4
+res = x**2+(p+q-y)/z #expression
+print("The result is",res)
+#2 part
+name = "codingal"
+age = 5
+if name == "chiamalite" or name=="codingal":
+    print("Hello")
+else:
+    print("Bye")
